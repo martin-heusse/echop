1f987b9f3645240da553be24f67692c2c59dfb20
Philippe Tran <philippe.tran2@gmail.com>
2013-06-05 18:59:22 +0200

    install bd ok

--- install_BD.sql
+++ install_BD.sql
@@ -19,15 +19,17 @@ CREATE TABLE utilisateur (
 	login varchar(255),
 	mot_de_passe varchar(255),
 	email varchar(255),
-	constraint pk_utilisateur primary key (id)
+
+	constraint pk_utilisateur primary key(id)
 );
 
 CREATE TABLE administrateur (
     id integer not null auto_increment,
     id_utilisateur integer not null,
 
-    constraint pk_administrateur primary key (id),
-    constraint fk_id_utilisateur foreign key (id_utilisateur)
+    constraint pk_administrateur primary key(id),
+
+    constraint fk_administrateur_1 foreign key(id_utilisateur)
     references utilisateur(id) on delete cascade
 );
 
@@ -37,15 +39,17 @@ CREATE TABLE campagne (
    date_debut date,
    etat boolean,
 
-   constraint pk_campagne primary key (id),
-   constraint fk_id_administrateur foreign key (id_administrateur)
+   constraint pk_campagne primary key(id),
+
+   constraint fk_campagne_1 foreign key(id_administrateur)
    references administrateur(id) on delete cascade
 );
 
 CREATE TABLE rayon (
    id integer not null auto_increment,
    nom varchar(255),
-   constraint pk_rayon primary key (id)
+
+   constraint pk_rayon primary key(id)
 );
 
 CREATE TABLE article (
@@ -59,8 +63,9 @@ CREATE TABLE article (
    description_courte varchar(255),
    description_longue varchar(255),
 
-   constraint pk_article primary key (id),
-   constraint fk_id_rayon foreign key (id_rayon) 
+   constraint pk_article primary key(id),
+
+   constraint fk_article_1 foreign key(id_rayon) 
    references rayon(id) on delete cascade
 );
 
@@ -69,14 +74,14 @@ CREATE TABLE fournisseur (
    id integer not null auto_increment,
    nom varchar(255),
 
-   constraint pk_fournisseur primary key (id)
+   constraint pk_fournisseur primary key(id)
 );
 
 CREATE TABLE tva (
        id integer not null auto_increment,
        valeur float,
        
-       constraint pk_tva primary key (id)
+       constraint pk_tva primary key(id)
 );
 
 CREATE TABLE article_fournisseur (
@@ -85,10 +90,12 @@ CREATE TABLE article_fournisseur (
    id_fournisseur integer not null,
    prix_article float,
 
-   constraint pk_article_fournisseur primary key (id),
-   constraint fk_id_article foreign key (id_article)
+   constraint pk_article_fournisseur primary key(id),
+
+   constraint fk_article_fournisseur_1 foreign key(id_article)
    references article(id) on delete cascade,
-   constraint fk_id_fournisseur foreign key (id_fournisseur) 
+
+   constraint fk_article_fournisseur_2 foreign key(id_fournisseur) 
    references fournisseur(id) on delete cascade
 );
 
@@ -101,39 +108,48 @@ CREATE TABLE article_campagne (
    seuil_min float,
    seuil_max float,
    prix_ttc float,
-   constraint pk_article_campagne primary key (id),
-   constraint fk_id_article foreign key (id_article) 
+
+   constraint pk_article_campagne primary key(id),
+
+   constraint fk_article_campagne_1 foreign key(id_article) 
    references article(id) on delete cascade,
-   constraint fk_id_campagne foreign key (id_campagne) 
+
+   constraint fk_article_campagne_2 foreign key(id_campagne) 
    references campagne(id) on delete cascade,
-   constraint fk_id_tva foreign key (id_tva) 
+
+   constraint fk_article_campagne_3 foreign key(id_tva) 
    references tva(id) on delete cascade
 );
 
--- CREATE TABLE campagne_rayon (
-   -- id integer not null auto_increment,
-   -- id_campagne integer not null,
-   -- id_rayon integer not null,
--- 
-   -- constraint pk_campagne_rayon primary key (id)
-   -- constraint fk_id_campagne foreign key (id_campagne) 
-   -- references campagne(id) on delete cascade,
-   -- constraint fk_id_rayon foreign key (id_rayon) 
-   -- references rayon(id) on delete cascade
--- );
+CREATE TABLE campagne_rayon (
+   id integer not null auto_increment,
+   id_campagne integer not null,
+   id_rayon integer not null,
 
--- CREATE TABLE commande (
-  -- id integer not null auto_increment,
-  -- id_article integer not null,
-  -- id_campagne integer not null,
-  -- id_utilisateur integer not null,
-  -- quantite integer,
--- 
-  -- constraint pk_commande primary key (id),
-  -- constraint fk_id_article foreign key (id_article) 
-  -- references article(id) on delete cascade,
-  -- constraint fk_id_campagne foreign key (id_campagne) 
-  -- references campagne(id) on delete cascade, 
-  -- constraint fk_id_utilisateur foreign key (id_utilisateur) 
-  -- references utilisateur(id) on delete cascade
--- );
+   constraint pk_campagne_rayon primary key(id),
+
+   constraint fk_campagne_rayon_1 foreign key(id_campagne) 
+   references campagne(id) on delete cascade,
+
+   constraint fk_campagne_rayon_2 foreign key(id_rayon) 
+   references rayon(id) on delete cascade
+);
+
+CREATE TABLE commande (
+  id integer not null auto_increment,
+  id_article integer not null,
+  id_campagne integer not null,
+  id_utilisateur integer not null,
+  quantite integer,
+
+  constraint pk_commande primary key(id),
+
+  constraint fk_commande_1 foreign key(id_article) 
+  references article(id) on delete cascade,
+
+  constraint fk_commande_2 foreign key(id_campagne) 
+  references campagne(id) on delete cascade, 
+
+  constraint fk_commande_3 foreign key(id_utilisateur) 
+  references utilisateur(id) on delete cascade
+);
