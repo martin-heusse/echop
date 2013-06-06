242bce0711ab4d5905cc412b028808b9a3dac3c8
Johann Yvetot <Johann.Yvetot@ensimag.imag.fr>
2013-06-06 11:35:50 +0200

    article begun

--- Model/Article.php
+++ Model/Article.php
@@ -1,11 +1,13 @@
-<?php
+<?Php
 class Article {
 
     /* Creaters */
 
-    public static function create($s_reference, $s_nom, $f_hauteur, $f_largeur, $f_poids, $f_prixGros, $f_prixDetail, $i_qteR, $i_qteV, $i_idCategorie) {
-        $sql_query = "insert into Article(reference, nom, hauteur, largeur, poids, prixGros, prixDetail, qteR, qteV, idCategorie) 
-            values('$s_reference', '$s_nom', '$f_hauteur', '$f_largeur', '$f_poids', '$f_prixGros', '$f_prixDetail', '$i_qteR', '$i_qteV', '$i_idCategorie')";
+    public static function create($i_id, $i_id_rayon, $s_nom, $s_code, $i_poids_paquet_fournisseur,
+				  $s_unite, $i_nb_paquet_colis, $s_description_courte, $s_description_longue) {
+        $sql_query = "insert into Article(id,id_rayon,nom,code,poids_paquet_fournisseur,unite,nb_paquet,colis,description_courte,description_longue) 
+            values('$i_id', '$i_id_rayon', '$s_nom','$s_code', '$i_poids_paquet_fournisseur',
+		   '$s_unite', '$i_nb_paquet_colis', '$s_description_courte', '$s_description_longue')";
         mysql_query($sql_query);
         $i_result = mysql_insert_id();
         return $i_result;
@@ -28,44 +30,11 @@ class Article {
         return $to_result;
     }
 
-    public static function getObjectsByIdCategorie($i_id) {
-        $sql_query = "select * from Article where idCategorie=$i_id";
-        $sql_tmp = mysql_query($sql_query);
-        $to_result = array();
-        while ($o_row = mysql_fetch_assoc($sql_tmp)) {
-            /* Sécurité */
-            foreach ($o_row as &$column) {
-                $column = htmlentities($column);
-            }
-            /* Création du résultat */
-            $to_result[] = $o_row;
-        }
-        /* Formattage des nombres */
-        foreach ($to_result as &$o_row) {
-            $o_row['hauteur']    = number_format($o_row['hauteur']   , 2, '.', ' ');
-            $o_row['largeur']    = number_format($o_row['largeur']   , 2, '.', ' ');
-            $o_row['poids']      = number_format($o_row['poids']     , 3, '.', ' ');
-            $o_row['prixGros']   = number_format($o_row['prixGros']  , 2, '.', ' ');
-            $o_row['prixDetail'] = number_format($o_row['prixDetail'], 2, '.', ' ');
-            $o_row['qteR']       = number_format($o_row['qteR']      , 0, '.', ' ');
-            $o_row['qteV']       = number_format($o_row['qteV']      , 0, '.', ' ');
-        }
-        return $to_result;
-    }
-
     public static function getObject($i_id) {
-        $sql_query = "select * from Article where id=$i_id";
+        $sql_query = "select * from article where id=$i_id";
         $sql_tmp = mysql_query($sql_query);
         $o_result = null;
         if ($o_row = mysql_fetch_assoc($sql_tmp)) {
-            /* Formattage des nombres */
-            $o_row['hauteur']    = number_format($o_row['hauteur']   , 2, '.', ' ');
-            $o_row['largeur']    = number_format($o_row['largeur']   , 2, '.', ' ');
-            $o_row['poids']      = number_format($o_row['poids']     , 3, '.', ' ');
-            $o_row['prixGros']   = number_format($o_row['prixGros']  , 2, '.', ' ');
-            $o_row['prixDetail'] = number_format($o_row['prixDetail'], 2, '.', ' ');
-            $o_row['qteR']       = number_format($o_row['qteR']      , 0, '.', ' ');
-            $o_row['qteV']       = number_format($o_row['qteV']      , 0, '.', ' ');
             /* Sécurité */
             foreach ($o_row as &$column) {
                 $column = htmlentities($column);
@@ -76,22 +45,96 @@ class Article {
         return $o_result;
     }
 
+    public static function getObjectsByIdRayon($i_id_rayon) {
+        $sql_query = "select * from Article where id_rayon=$i_id_rayon";
+        $sql_tmp = mysql_query($sql_query);
+        $to_result = array();
+        while ($o_row = mysql_fetch_assoc($sql_tmp)) {
+            /* Sécurité */
+            foreach ($o_row as &$column) {
+                $column = htmlentities($column);
+            }
+            /* Création du résultat */
+            $to_result[] = $o_row;
+        }
+        return $to_result;
+    }
+
+    public static function getNom($i_id) {
+        $sql_query = "select nom from article where id=$i_id";
+        $sql_tmp = mysql_query($sql_query);
+        $s_result = null;
+        if ($o_row = mysql_fetch_assoc($sql_tmp)) {
+            /* Sécurité et création du résultat */
+            $s_result = htmlentities($o_row['nom']);
+        }
+        return $s_result;
+    }
+
+    public static function getIdCampagne($i_id) {
+        $sql_query = "select id_campagne from article where id=$i_id";
+        $sql_tmp = mysql_query($sql_query);
+        $s_result = null;
+        if ($o_row = mysql_fetch_assoc($sql_tmp)) {
+            /* Sécurité et création du résultat */
+            $s_result = htmlentities($o_row['login']);
+        }
+        return $s_result;
+    }
+
+    public static function getIdCampagne($i_id) {
+        $sql_query = "select id_campagne from article where id=$i_id";
+        $sql_tmp = mysql_query($sql_query);
+        $s_result = null;
+        if ($o_row = mysql_fetch_assoc($sql_tmp)) {
+            /* Sécurité et création du résultat */
+            $s_result = htmlentities($o_row['login']);
+        }
+        return $s_result;
+    }
+
+    public static function getIdCampagne($i_id) {
+        $sql_query = "select id_campagne from article where id=$i_id";
+        $sql_tmp = mysql_query($sql_query);
+        $s_result = null;
+        if ($o_row = mysql_fetch_assoc($sql_tmp)) {
+            /* Sécurité et création du résultat */
+            $s_result = htmlentities($o_row['login']);
+        }
+        return $s_result;
+    }
+
+
     /* Setters */
 
-    public static function set($i_id, $s_reference, $s_nom, $f_hauteur, $f_largeur, $f_poids, $f_prixGros, $f_prixDetail, $i_qteR, $i_qteV, $i_idCategorie) {
-        $sql_query = "update Article set reference='$s_reference', nom='$s_nom', hauteur='$f_hauteur', largeur='$f_largeur', 
-            poids='$f_poids', prixGros='$f_prixGros', prixDetail='$f_prixDetail', qteR='$i_qteR', qteV='$i_qteV', idCategorie='$i_idCategorie' 
+    public static function set($i_id, $i_id_campagne, $i_id_rayon) {
+      $sql_query = "update article set id_campagne = '$i_id_campagne',
+      id_rayon='$i_id_rayon', where id=$i_id";
+      $b_result =  mysql_query($sql_query);
+      return $b_result;
+    }
+
+    public static function setIdCampagne($i_id, $i_id_campagne) {
+        $sql_query = "update article set id_campagne='$i_id_campagne' 
             where id=$i_id";
         $b_result =  mysql_query($sql_query);
-        return $i_result;
+        return $b_result;
     }
+
+    public static function setIdRayon($i_id, $i_id_rayon) {
+        $sql_query = "update article set id_rayon='$i_id_rayon' 
+            where id=$i_id";
+        $b_result =  mysql_query($sql_query);
+        return $b_result;
+    }
+
 
     /* Deleters */
 
     public static function delete($i_id) {
-        $sql_query = "delete from Article where id=$i_id";
+        $sql_query = "delete from article where id=$i_id";
         $b_result =  mysql_query($sql_query);
-        return $i_result;
+        return $b_result;
     }
 }
 ?>
