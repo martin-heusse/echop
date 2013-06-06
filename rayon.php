8600fe9e3097ab1cf172b6e2dc38f470b78bc15d
Philippe Tran <philippe.tran2@gmail.com>
2013-06-06 17:24:37 +0200

    add

---
+++ rayon.php
@@ -0,0 +1,22 @@
+<?php
+require_once('def.php');
+require_once('Model/Rayon.php');
+require_once('Model/Administrateur.php');
+require_once('Model/Utilisateur.php');
+
+class RayonController extends Controller {
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    /* Code Gilou */
+
+    /* */
+
+    public function defaultAction() {
+        header('Location: '.root.'/connexion.php/connexion');
+    }
+}
+new RayonController();
+?>
