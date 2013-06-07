345f58463c2f8653e8c13e7c8d9957d692453833
Philippe Tran <philippe.tran2@gmail.com>
2013-06-07 15:58:13 +0200

    menu

--- Layouts/menu.php
+++ Layouts/menu.php
@@ -12,17 +12,20 @@ if (Utilisateur::isLogged()) {
         <li><a href="<?php echo root ?>/index.php">Accueil</a></li>
         <li><a href="<?php echo root ?>/connexion.php/deconnexion">Se déconnecter</a></li>
     </ul>
-    <!-- Menu utilisateur -->
-    <ul>
-        <li><a href="<?php echo root ?>/index.php">lien utilisateur</a></li>
-        <li><a href="<?php echo root ?>/index.php">lien utilisateur</a></li>
-        <li><a href="<?php echo root ?>/index.php">lien utilisateur</a></li>
-    </ul>
+<?php
+    if(Administrateur::isAdministrateur($_SESSION['idUtilisateur'])) {
+?>
     <!-- Menu administrateur -->
     <ul>
-        <li><a href="<?php echo root ?>/index.php">lien admin</a></li>
-        <li><a href="<?php echo root ?>/index.php">lien admin</a></li>
-        <li><a href="<?php echo root ?>/index.php">lien admin</a></li>
+        <li><a href="<?php echo root ?>/rayon.php/gererRayon">Gérer les rayon</a></li>
+    </ul>
+<?php
+    }
+?>
+    <!-- Menu utilisateur -->
+    <ul>
+        <li><a href="<?php echo root ?>/article.php/commanderArticle">Commander des articles</a></li>
+        <li><a href="<?php echo root ?>/commande.php/maCommande">Ma commande</a></li>
     </ul>
 <?php
 } else {
@@ -31,11 +34,6 @@ if (Utilisateur::isLogged()) {
         <li><a href="<?php echo root ?>/index.php">Accueil</a></li>
         <li><a href="<?php echo root ?>/connexion.php/connexion">Se connecter</a></li>
     </ul>
-<?php
-}
-if(Administrateur::isAdministrateur($_SESSION['idUtilisateur'])) {
-?>
-    <p>Vous êtes administrateur.</p>
 <?php
 }
 ?>
