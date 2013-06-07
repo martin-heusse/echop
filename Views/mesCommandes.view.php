42301e7dc7f79549eb06db5c8ebe7605453e9ea9
Aurore Bellas <bellasa@ensibm.imag.fr>
2013-06-07 17:54:30 +0200

    javascript

--- Views/mesCommandes.view.php
+++ Views/mesCommandes.view.php
@@ -2,7 +2,7 @@
 <?php
 if ($to_commande != null) {
 ?>
-<table>
+<table id="t_article">
 <tr>
     <th>Produit</th>
     <th>Description courte</th>
@@ -13,8 +13,10 @@ if ($to_commande != null) {
     <th>Prix TTC</th>
     <th>Prix TTC unitaire (au kilo/litre)</th>
     <th>Poids unitaire que le client peut commander</th>
+    <th>Unite</th>
     <th>Quantité</th>
     <th>Quantité totale commandée</th>
+    <th>Unite</th>
     <th>Total TTC</th>
     
 </tr>
@@ -31,8 +33,10 @@ foreach($to_commande as $o_produit) {
     <td><?php echo $o_produit['prix_ttc'] ?></td>
     <td><?php echo $o_produit['prix_unitaire'] ?></td>
     <td><?php echo $o_produit['poids_paquet_client'] ?></td>
-    <td><?php echo $o_produit['quantite'] ?></td>
+    <td><?php echo $o_produit['unite'] ?></td>
+    <td onKeyPress="calculPoidsTotal()"><input type="text" value="<?php echo $o_produit['quantite'] ?>"/></td>
     <td><?php echo $o_produit['quantite_totale'] ?></td>
+    <td><?php echo $o_produit['unite'] ?></td>
     <td><?php echo $o_produit['total_ttc'] ?></td>
 </tr>
 <?php
@@ -46,3 +50,12 @@ foreach($to_commande as $o_produit) {
 <?php
 }
 ?>
+<script type="text/javascript">
+function calculPoidsTotal(){
+  var poidsTotal = 0.0;
+  var test = event.srcElement;
+  // quantité est la 10eme colonne
+  document.write(test);
+  var quantite = parseFloat(document.getElementById("t_article").rows[this.rowIndex].cells[10].innerHTML); 
+} 
+</script>
