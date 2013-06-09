2cadc03ff856696b202f9d9af301a44073287c68
Philippe Tran <philippe.tran2@gmail.com>
2013-06-10 00:33:39 +0200

    accueil

---
+++ accueil.php
@@ -0,0 +1,37 @@
+<?php
+require_once('def.php');
+require_once('Model/Utilisateur.php');
+
+/*
+ * Gère la page d'accueil.
+ */
+class AccueilController extends Controller {
+
+    /*
+     * Constructeur.
+     */
+    public function __construct() {
+        parent::__construct();
+    }
+
+    /*
+     * Affiche la page d'accueil.
+     */
+    function accueil() {
+        /* Authentication required */
+        if (!Utilisateur::isLogged()) {
+            $this->render('authenticationRequired');
+            return;
+        }
+        $this->render('accueil');
+    }
+
+    /*
+     * Page par défaut.
+     */
+    public function defaultAction() {
+        header('Location: '.root.'/accueil.php/accueil');
+    }
+}
+new AccueilController();
+?>
