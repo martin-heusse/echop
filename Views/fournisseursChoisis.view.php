972c1f43bf2656ea5ee0ac407fedf1b36f7628c4
Philippe Tran <philippe.tran2@gmail.com>
2013-06-09 20:39:34 +0200

    e

---
+++ Views/fournisseursChoisis.view.php
@@ -0,0 +1,11 @@
+<h1>Fournisseurs choisis</h1>
+
+<ul>
+<?php
+foreach ($to_fournisseur as $o_fournisseur) {
+?>
+<li><a href="<?php echo root ?>/articleCampagne.php/commandeFournisseur?idFournisseur=<?php echo $o_fournisseur['id'] ?>"><?php echo $o_fournisseur['nom'] ?></a></li>
+<?php
+}
+?>
+</ul>
