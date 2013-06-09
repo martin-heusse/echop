d0ff859e84bc9c43afdb1ca92f0308b1c6a5d74b
Philippe Tran <philippe.tran2@gmail.com>
2013-06-09 18:24:24 +0200

    d

---
+++ Views/articlesCommandEs.view.php
@@ -0,0 +1,11 @@
+<h1>Articles commandés</h1>
+
+<ul>
+<?php
+foreach ($to_article as $o_article) {
+?>
+    <li><a href="<?php echo root ?>/commande.php/utilisateursAyantCommandECetArticle?idArticle=<?php echo $o_article['id_article']?>"><?php echo $o_article['nom']?></a></li>
+<?php
+}
+?>
+</ul>
