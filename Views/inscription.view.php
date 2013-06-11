6be3bb54388c073a0c8f9a9a35852a769a35ba79
Johann Yvetot <Johann.Yvetot@ensimag.imag.fr>
2013-06-11 11:44:54 +0200

    fuck my computer

--- Views/inscription.view.php
+++ Views/inscription.view.php
@@ -1,32 +1,39 @@
 <h1>Inscription</h1>
 
 <?php 
-    if ($i_errReg == 0) {
+    if ($i_errReg != 0) {
 ?>
     <form name="formulaire" class="form_connexion"
-    action="<?php echo root ?>/inscription.php"
+    action="<?php echo root ?>/inscription.php/inscription"
     enctype="multipart/form-data"
     method="post">
 
         <p><span class="form_col"><label>Login</label></span><input type="text"
-        name="login" required/>
+        name="login" required/></p>
 <?php 
         if ($i_errLogin == 1) {
 ?>
-            Login déjà existant</p>
+    <p>Login déjà existant"</p>
 <?php
         }
 ?>
-
+        
     <p><span class="form_col"><label>Mot de passe</label></span><input type="password" name="motDePasse" required/></p>
     <p><span class="form_col"><label>Email</label></span><input type="text" name="email" required/></p>
-    <input type="sumbit" value="S'enregistrer"/>
+    <input type="submit" value="S'enregistrer"/>
 </form>
 
 <?php
     } else {
 ?>
-        <p> Votre inscription a été soumis à la validation par un administrateur.</p>
+        <p> Votre inscription a été soumise à la validation par un administrateur.</p>
 <?php
-    }
+    }  
 ?>
+
+<p> errReg :<?php echo $i_errReg;?></p>
+<p> errLogin : <?php echo $i_errLogin;?></p>
+<p> fake : <?php echo $fake; ?></p>
+<p> login : <?php echo $s_login; ?></p>
+<p> mdp : <?php echo $s_passwd; ?></p>
+<p> email : <?php echo $s_email; ?></p>
