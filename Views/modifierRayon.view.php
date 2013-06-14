abcf87163933763cf7d7d543c3ee073ca8520c78
Johann Yvetot <Johann.Yvetot@ensimag.imag.fr>
2013-06-14 09:46:04 +0200

    add nom rayon

--- Views/modifierRayon.view.php
+++ Views/modifierRayon.view.php
@@ -27,6 +27,7 @@ method="get">
 action="<?php echo root ?>/rayon.php/modifierRayon"
 enctype="multipart/form-data"
 method="post">
+    <p> Ancien nom : <?php echo $s_Rayon ?> </p>
     <p><span class="form_col"><label>Nouveau nom du rayon</label></span><input type="text" name="newNomRayon" required/></p>
     <p><span class="form_col"><label></label></span><input type="hidden" name="idRayon" value="<?php echo $i_idRayon ?>"</p>
 
