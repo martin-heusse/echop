2bcbe9b883cdb3c824fe6435c579749d415f2509
Johann Yvetot <Johann.Yvetot@ensimag.imag.fr>
2013-06-15 11:43:53 +0200

    woot

--- rayon.php
+++ rayon.php
@@ -59,7 +59,7 @@ class RayonController extends Controller {
             $i_oldRayonSet = 1;
             $i_idRayon = $_GET['idRayon'];
             $s_Rayon = Rayon::getNom($i_idRayon); 
-            $f_marge = Rayonn::getMarge($i_idRayon);
+            $f_marge = Rayon::getMarge($i_idRayon);
             $this->render('modifierRayon',compact('f_marge','s_Rayon','i_idRayon','i_errNewName','i_oldRayonSet','to_rayon'));
         }
 
