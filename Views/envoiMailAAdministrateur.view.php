df0eadc2ceb5f929c04c0d1559c00c910eae8d31
Aurore Bellas <bellasa@ensibm.imag.fr>
2013-06-18 14:09:03 +0200

    ok

---
+++ Views/envoiMailAAdministrateur.view.php
@@ -0,0 +1,25 @@
+<p>
+    <a class="action_navigation" href="<?php echo root ?>">Retour à l'accueil</a>
+</p>
+<h1>Envoyer un mail aux administrateurs</h1>
+
+<?php
+    if ($i_emailSent == 0) {
+?>
+
+    <form name="formulaire" 
+    action="<?php echo root ?>/utilisateur.php/envoiMailAAdministrateur"
+    enctype="multipart/form-data"
+    method="post">
+    
+    <p><span><label>Sujet :</label></span><input type="text" name="subject" required/></p>
+    <br><textarea rows ='10' cols='70' name='message'></textarea></br>
+    <input type="submit" value="Envoyer">
+    </form>
+<?php 
+    } else {
+?>
+    <p> Le message a été envoyé aux utilisateurs.</p>
+<?php
+    }
+?>
