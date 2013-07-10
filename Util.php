ba1a434631bbfba95843d5b4d456d1682fc79396
Martin Heusse <martin.heusse@imag.fr>
2013-07-10 19:33:51 +0200

    Ajout email_owner dans constants.php, et utilisation

--- Util.php
+++ Util.php
@@ -8,7 +8,7 @@ class Util {
         /* Header */
         $s_header  = 'MIME-Version: 1.0'."\r\n";
         $s_header .= 'Content-type: text/html; charset=utf-8'."\r\n";
-        $s_header .= 'From: <philippe.tran@ensimag.fr>'."\r\n"; // a changer
+        $s_header .= 'From:'.email_owner."\r\n"; // a changer
         /* Contenu */
         $s_contenu  = "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\t";
         $s_contenu .= "<p>".$s_message."</p>\n" ;
--- constants-template.php
+++ constants-template.php
@@ -4,4 +4,5 @@ define('db_username','root');
 define('db_name','BdEchoppe');
 define('db_pwd','XXX');
 define('url_site','http://...'); // site root URL, no trailing "/"
+define('email_owner','echoppe@email.com'); 
 ?>
