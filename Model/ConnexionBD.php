6f3be46b265a6a1652cea1916dc68aff7ea40ef1
Martin Heusse <martin.heusse@imag.fr>
2013-07-11 15:48:03 +0200

    Ajout Model/ConnexionBD.php, les infos a changer sont maintenant dans constants.php

---
+++ Model/ConnexionBD.php
@@ -0,0 +1,13 @@
+<?php
+// Connexion à la BD
+function connexion_bd() {
+    $connect = mysql_connect(db_host, db_username,db_pwd);
+    if (!$connect) {
+        die("Erreur de connexion au serveur");
+    }
+    mysql_select_db(db_name) //??
+    or die("Erreur de connexion à la base de données");
+    define('connect', $connect);
+    //return $connect;
+}
+?>
