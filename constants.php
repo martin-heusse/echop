ab48a087575d1d32ee488ac48b3d7cee36c9d299
Your Name <you@example.com>
2014-05-27 10:55:37 +0200

    ok

--- constants.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('db_host','');
-define('db_username','');
-define('db_name','');
-define('db_pwd','');
-define('url_site','');
-define('email_owner','');
-?>
---
+++ echoppe/constants.php
@@ -0,0 +1,8 @@
+<?php
+define('db_host','localhost');
+define('db_username','yoyo');
+define('db_name','BdEchoppe');
+define('db_pwd','yoyo');
+define('url_site','http://localhost/~maxime-blanloeil/echoppe');
+define('email_owner','');
+?>
