d6e37fde8c72df1d32dc4a00911564f2c70ab764
Your Name <you@example.com>
2014-05-28 11:24:50 +0200

    ok

--- echoppe/constants.php
+++ echoppe/constants.php
@@ -1,8 +1,8 @@
 <?php
 define('db_host','localhost');
-define('db_username','root');
+define('db_username','yoyo');
 define('db_name','BdEchoppe');
-define('db_pwd','12SCtKcI');
-define('url_site','http://localhost/~Nabil/echoppe/echoppe');
-define('email_owner','');
+define('db_pwd','yoyo');
+define('url_site','http://localhost/~maxime-blanloeil/echoppe/');
+define('email_owner','nabil_tazi@hotmail.fr');
 ?>
