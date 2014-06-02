11d6ac55a442410f8cb411d61c59c325586c00fd
Johann SIGRIST <Johann.Sigrist@phelma.grenoble-inp.fr>
2014-06-02 09:22:47 +0200

    nique le git

--- echoppe/constants.php
+++ echoppe/constants.php
@@ -1,8 +1,8 @@
 <?php
 define('db_host','localhost');
 define('db_username','root');
-define('db_name','BdEchoppe');
-define('db_pwd','12SCtKcI');
-define('url_site','http://localhost/~Nabil/echoppe/echoppe');
-define('email_owner','nabil_tazi@hotmail.fr');
+define('db_name','bdechoppe');
+define('db_pwd','root');
+define('url_site','http://localhost/~johann/echoppe/');
+define('email_owner','johannsigrist@gmail.com');
 ?>
