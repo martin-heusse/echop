f03e313855247936d69c2365750004a4a3a92566
Johann SIGRIST <Johann.Sigrist@phelma.grenoble-inp.fr>
2014-06-02 09:45:57 +0200

    ok

--- echoppe/constants.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('db_host','localhost');
-define('db_username','yoyo');
-define('db_name','BdEchoppe');
-define('db_pwd','yoyo');
-define('url_site','http://localhost/~maxime-blanloeil/echoppe/');
-define('email_owner','nabil_tazi@hotmail.fr');
-?>
