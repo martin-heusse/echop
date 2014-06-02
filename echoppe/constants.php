4c8ad70b4a24ac9f911cf4b1eb02d97223719ea7
Your Name <you@example.com>
2014-06-02 09:30:46 +0200

    ok

--- echoppe/constants.php
+++ echoppe/constants.php
@@ -1,16 +1,8 @@
 <?php
 define('db_host','localhost');
-<<<<<<< HEAD
-define('db_username','root');
-define('db_name','bdechoppe');
-define('db_pwd','root');
-define('url_site','http://localhost/~johann/echoppe/');
-define('email_owner','johannsigrist@gmail.com');
-=======
 define('db_username','yoyo');
 define('db_name','BdEchoppe');
 define('db_pwd','yoyo');
 define('url_site','http://localhost/~maxime-blanloeil/echoppe/');
 define('email_owner','nabil_tazi@hotmail.fr');
->>>>>>> 88d17d3c38ba7eb28aad98c9b1468f84c93bcd59
 ?>
